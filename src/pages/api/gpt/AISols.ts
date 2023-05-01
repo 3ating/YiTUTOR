3964f785a16bdf4faac423475e8a93d6c1c63155
Yiting <a0958165875@gmail.com>
2023-05-01 15:43:45 +0800

    Find API_KEY error

--- src/pages/api/gpt/AISols.ts
+++ src/pages/api/gpt/AISols.ts
@@ -12,19 +12,9 @@ export default async function handler(
         setHeader: (arg0: string, arg1: string) => void;
     }
 ) {
-    // 設置CORS頭部
-    res.setHeader('Access-Control-Allow-Origin', 'https://yitutor.vercel.app/');
-    res.setHeader('Access-Control-Allow-Methods', 'POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-    // 處理OPTIONS請求
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
-
     if (req.method === 'POST') {
         const API_KEY = process.env.OPENAI_API_KEY;
+        console.log('API_KEY:', API_KEY);
         const apiRequestBody = req.body;
 
         try {
