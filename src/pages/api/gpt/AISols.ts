2e0079fae8a5b529b3444f8e39a963348a78da83
Yiting <a0958165875@gmail.com>
2023-05-04 15:35:10 +0800

    Try to fix gpt req error problems

--- src/pages/api/gpt/AISols.ts
+++ src/pages/api/gpt/AISols.ts
@@ -1,4 +1,4 @@
-// import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 
 // export default async function handler(
 //     req: { method: string; body: any },
@@ -38,12 +38,38 @@
 //     }
 // }
 
+// export default async function handler(
+//     req: { body: any },
+//     res: { status: (arg0: number) => { (): any; new (): any; json: { (arg0: any): void; new (): any } } }
+// ) {
+//     const API_KEY = process.env.OPENAI_API_KEY;
+//     const apiRequestBody = req.body;
+
+//     const response = await fetch('https://api.openai.com/v1/chat/completions', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             Authorization: `Bearer ${API_KEY}`,
+//         },
+//         body: JSON.stringify(apiRequestBody),
+//     });
+
+//     const data = await response.json();
+//     res.status(200).json(data);
+// }
+
 export default async function handler(
     req: { body: any },
     res: { status: (arg0: number) => { (): any; new (): any; json: { (arg0: any): void; new (): any } } }
 ) {
     const API_KEY = process.env.OPENAI_API_KEY;
     const apiRequestBody = req.body;
+
+    // Check if apiRequestBody is a valid JSON object
+    if (typeof apiRequestBody !== 'object' || apiRequestBody === null) {
+        res.status(400).json({ error: 'Invalid JSON object in the request body.' });
+        return;
+    }
 
     const response = await fetch('https://api.openai.com/v1/chat/completions', {
         method: 'POST',
